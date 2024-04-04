ad43430399e3dd0c67a5e6108c39a14e149b158f
Herbert Nordson <herbert_nordson@hotmail.com>
2024-04-04 18:32:09 -0300

    feat: create new user

--- flamme-backend/src/main/java/com/flamme/flammebackend/controllers/UserController.java
+++ flamme-backend/src/main/java/com/flamme/flammebackend/controllers/UserController.java
@@ -1,19 +1,53 @@
 package com.flamme.flammebackend.controllers;
 
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.flamme.flammebackend.entities.User;
+import com.flamme.flammebackend.services.UserService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private UserService service;
+
+    // GET
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User("Flamme", "flammevelas95@gmail.com", "MarinaRabelo!1995", "");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = service.findById(id);
+
+        return ResponseEntity.ok().body(user);
+    }
+
+    // POST
+    @PostMapping
+    public ResponseEntity<Object> create(@Valid @RequestBody User user){
+        try {
+           var result = this.service.create(user);
+            
+           return ResponseEntity.ok().body(result);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }
--- flamme-backend/src/main/java/com/flamme/flammebackend/entities/User.java
+++ flamme-backend/src/main/java/com/flamme/flammebackend/entities/User.java
@@ -12,16 +12,20 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import jakarta.validation.constraints.Email;
+import lombok.Data;
 
+@Data
 @Entity
 @Table(name = "tb_user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @Length(min = 3)
     private String name;
@@ -29,56 +33,24 @@ public class User implements Serializable {
     @Email(message = "O campo [email] deve conter um e-mail válido")
     private String email;
 
-    @Length(min = 8, max = 32)
+    @Length(min = 8, max = 100)
     private String password;
 
-    @Length(min = 9, max = 12)
+    @Length(min = 11, max = 15)
     private String phone;
 
     @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime createdAt;
+
+    public User() {
+    }
 
     public User(String name, String email, String password, String phone) {
         super();
         this.name = name;
         this.email = email;
         this.password = password;
-        this.phone = phone;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
         this.phone = phone;
     }
 
