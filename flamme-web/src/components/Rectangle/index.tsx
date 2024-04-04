ae2cc37e57fbffceadbc1c95ed08589f55eb5e15
Rafael Penido <rafaelpenido1998@gmail.com>
2024-04-04 00:08:58 -0300

    melhorando css page addProduct

--- flamme-web/src/components/Rectangle/index.tsx
+++ flamme-web/src/components/Rectangle/index.tsx
@@ -1,11 +1,11 @@
 const Rectangle = () => {
   return (
-    <div className="flex flex-row justify-center items-center gap-3">
-    <div className="w-20 h-20 rounded-5 bg-primary text-white text-center">+</div>
-    <div className="w-20 h-20 rounded-5 bg-primary"></div>
-    <div className="w-20 h-20 rounded-5 bg-primary"></div>
-    <div className="w-20 h-20 rounded-5 bg-primary"></div>
-</div>
+    <div className="flex justify-center items-center gap-2 mt-2">
+        <div className="w-20 h-16 bg-primary rounded-md flex justify-center items-center text-white text-4xl">+</div>
+        <div className="w-20 h-16 rounded-md border border-dashed border-gray-300"></div>
+        <div className="w-20 h-16 rounded-md border border-dashed border-gray-300"></div>
+        <div className="w-20 h-16 rounded-md border border-dashed border-gray-300"></div>
+    </div>
 
   );
 };
