9e33ba063dc420e1dfe614b1896c269de6584613
rapa2302 <lasrosolem@sga.pucminas.br>
2024-06-07 10:47:38 -0300

    feat: integração tela de informação de usuário

--- flamme-web/src/screens/RegisterInformation/index.tsx
+++ flamme-web/src/screens/RegisterInformation/index.tsx
@@ -3,8 +3,34 @@ import { Text } from "../../components/Text";
 import GrayBorderTop from "../../components/GrayBorderTop/index.tsx";
 import ButtonNavBarPerfil from "../../components/ButtonNavBarPerfil";
 import { Link } from "react-router-dom";
+import { useStoreContext } from "../../contexts/index.tsx";
+import { userController } from "../../services/request/user.ts";
+import { useEffect, useState } from "react";
+import { IUser } from "../../utils/interfaces.ts";
 
 function RegisterInformation() {
+  const [userData, setUserData] = useState<IUser>({} as IUser);
+  const { user } = useStoreContext();
+  const { getForId: getUserId, patch: patchUser } = userController();
+
+  async function fetchData() {
+    const id = localStorage.getItem("id") ?? 0;
+    const response = await getUserId(Number(id));
+    const data: IUser = {
+      id: response.id,
+      name: response.name,
+      email: response.email,
+      password: user.password,
+      phone: response.phone,
+    };
+    console.log(data);
+    setUserData(data);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="RegisterInformation">
       <div className="flex justify-center mt-4 mb-4">
@@ -20,14 +46,15 @@ function RegisterInformation() {
         </Link>
       </div>
 
-
       <form action="" className="mx-6">
         <div className="flex flex-col mt-10">
           <label htmlFor="">Nome</label>
           <input
             className="w-full px-4 py-4 mt-2 border rounded-md text-xs"
             type="text"
-            placeholder="Usuário da Silva"
+            name="name"
+            value={userData?.name}
+            placeholder={userData?.name}
           />
         </div>
 
@@ -36,7 +63,9 @@ function RegisterInformation() {
           <input
             className="w-full px-4 py-4 mt-2 border rounded-md text-xs"
             type="tel"
-            placeholder="(31) 91234-5678"
+            name="phone"
+            placeholder={userData?.phone}
+            value={userData?.phone}
           />
         </div>
 
@@ -45,7 +74,9 @@ function RegisterInformation() {
           <input
             className="w-full px-4 py-4 mt-2 border rounded-md text-xs"
             type="email"
-            placeholder="email.do.usuario@gmail.com"
+            name="email"
+            placeholder={userData?.email}
+            value={userData?.email}
           />
         </div>
 
@@ -53,13 +84,14 @@ function RegisterInformation() {
         <div className="flex flex-col mt-5">
           <label htmlFor="">Senha</label>
           <input
+            disabled
             className="w-full px-4 py-4 mt-2 border rounded-md text-xs"
             type="password"
             placeholder="********"
           />
         </div>
 
-        <div className="mt-3 text-slate-500">
+        {/* <div className="mt-3 text-slate-500">
           <strong>
             <Text text="Sua senha deve conter:" />
           </strong>
@@ -77,7 +109,7 @@ function RegisterInformation() {
               placeholder="********"
             />
           </div>
-        </div>
+        </div> */}
       </form>
 
       <ButtonNavBarPerfil />
